f9cd9786d4666ec4a4514a042219b557806a77f9
yangzy <306525121@qq.com>
2025-10-18 00:01:35 +0800

    项目初始化

---
+++ src/test/java/cn/codeyang/yang/YangApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codeyang.yang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
